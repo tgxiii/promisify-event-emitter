8c377174e47e32e6696cc8494d1438951993a839
Teotimo Garcia <teotimo.garcia@vixlet.com>
2015-05-27 14:14:05 -0700

    Add correct description for eventEmitter parameter

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var Promise = require('bluebird');
 /**
  * Takes an EventEmitter and converts it into a function that returns a promise.
  * @param {object} params                 Object that contains parameters
- * @param {object} params.eventEmitter    The EventEmitter object
+ * @param {object} params.eventEmitter    Function that returns an event emitter
  * @param {object} params.applyThis       This function performs a params.eventEmitter.apply
  *                                        If the "this" object needs to be different, set it here.
  * @param {object} params.listeners       List of event listeners.
@@ -26,14 +26,57 @@ var promisifyEventEmitter = function(params) {
   }
   return function() {
     var args = arguments;
+    var applyThis = params.applyThis || params.eventEmitter;
+    var e = params.eventEmitter.apply(applyThis, args);
+    var error;
+    var result;
+    var hasDataListener = false;
+    var hasErrorListener = false;
+    var hasEndListener = false;
+    var addListener = function (listener, method) {
+      e[method](listener.name, function (arg) {
+        /** @namespace listener.isResult */
+        if (listener.isResult) {
+          hasDataListener = true;
+          result = arg;
+        }
+        /** @namespace listener.isError */
+        if (listener.isError) {
+          hasErrorListener = true;
+          error = arg;
+        }
+        listener[method](arg);
+        if (listener.name === 'end') {
+          hasEndListener = true;
+          endFunction();
+        }
+      });
+    };
+    params.listeners.forEach(function(listenerOptions) {
+      if (!listenerOptions.name) {
+        throw new Error('listener must have a name');
+      }
+      /** @namespace listenerOptions.on */
+      /** @namespace listenerOptions.once */
+      if (listenerOptions.on) {
+        addListener(listenerOptions, 'on');
+      } else if (listenerOptions.once) {
+        addListener(listenerOptions, 'once');
+      } else {
+        throw new Error('listener must either have an "on" or "once" function');
+      }
+    });
+    if (!hasDataListener) {
+      e.once('data', function(data) {
+        result = data;
+      });
+    }
+    if (!hasErrorListener) {
+      e.on('error', function(err) {
+        error = err;
+      });
+    }
     return new Promise(function(resolve, reject) {
-      var applyThis = params.applyThis || params.eventEmitter;
-      var e = params.eventEmitter.apply(applyThis, args);
-      var error;
-      var result;
-      var hasDataListener = false;
-      var hasErrorListener = false;
-      var hasEndListener = false;
       var endFunction = function() {
         if (error) {
           return reject(error);
@@ -41,49 +84,6 @@ var promisifyEventEmitter = function(params) {
         var resolveThis = params.transform && result ? params.transform(result) : result;
         return resolve(resolveThis);
       };
-      var addListener = function (listener, method) {
-        e[method](listener.name, function (arg) {
-          /** @namespace listener.isResult */
-          if (listener.isResult) {
-            hasDataListener = true;
-            result = arg;
-          }
-          /** @namespace listener.isError */
-          if (listener.isError) {
-            hasErrorListener = true;
-            error = arg;
-          }
-          listener[method](arg);
-          if (listener.name === 'end') {
-            hasEndListener = true;
-            endFunction();
-          }
-        });
-      };
-      params.listeners.forEach(function(listenerOptions) {
-        if (!listenerOptions.name) {
-          throw new Error('listener must have a name');
-        }
-        /** @namespace listenerOptions.on */
-        /** @namespace listenerOptions.once */
-        if (listenerOptions.on) {
-          addListener(listenerOptions, 'on');
-        } else if (listenerOptions.once) {
-          addListener(listenerOptions, 'once');
-        } else {
-          throw new Error('listener must either have an "on" or "once" function');
-        }
-      });
-      if (!hasDataListener) {
-        e.once('data', function(data) {
-          result = data;
-        });
-      }
-      if (!hasErrorListener) {
-        e.on('error', function(err) {
-          error = err;
-        });
-      }
       if (!hasEndListener) {
         e.once('end', endFunction);
       }
